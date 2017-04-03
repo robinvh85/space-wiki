c65fd38f1a70fbf60e289c3e86b09be54ca339ad
robinvh85 <hoangviet.vh@gmail.com>
2017-04-03 18:17:04 +0700

    Add devise

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,4 +1,6 @@
 class SpacesController < ApplicationController
-	def index
+	before_action :authenticate_user!
+
+	def index		
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170403031443) do
+ActiveRecord::Schema.define(version: 20170403044107) do
 
   create_table "tags", force: :cascade do |t|
     t.string   "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170403031443) do
     t.datetime "updated_at",             null: false
     t.integer  "topic_id",   default: 0
     t.integer  "parent_id",  default: 0
+    t.integer  "level",      default: 0
   end
 
 end
