f2e50609bb4894e65e1179700fd5ea4ee77fca72
robinvh85 <hoangviet.vh@gmail.com>
2017-04-19 15:04:41 +0700

    Change edit and new form are not use popup

--- app/assets/javascripts/components/markdown.js
+++ app/assets/javascripts/components/markdown.js
@@ -1,12 +1,47 @@
 Vue.component('markdown', {
-	template: "<div :style='style'><textarea :id='markdown_id'></textarea></div>",
+	template: `
+	<div>		
+		<div :style='style'><textarea :id='markdown_id'></textarea></div>	
+		<div class="modal" :class="{ 'is-active': is_upload }">
+		  <div class="modal-background"></div>
+		  <div class="modal-card">
+		    <header class="modal-card-head">
+		      <p class="modal-card-title">Modal title</p>
+		      <button class="delete"></button>
+		    </header>
+		    <section class="modal-card-body">
+		      <!-- Content ... -->
+		    </section>
+		    <footer class="modal-card-foot">
+		      <a class="button is-success" @click="save">Save changes</a>
+		      <a class="button" @click="cancel">Cancel</a>
+		    </footer>
+		  </div>
+		</div>
+	</div>
+	`,
 	props: ["id", "content", "style"],	
 	data: function() {
 		return {
-			simplemde: null
+			simplemde: null,
+			is_upload: false,
+			editor: null
 		};
 	},
 	methods: {
+		cancel: function(){
+			this.is_upload = false;
+		},
+		save: function() {
+			var cm = this.editor.codemirror;
+	    var output = '';
+	    var selectedText = cm.getSelection();
+	    var text = selectedText || 'placeholder';
+
+	    output = '!!' + text + '!!';
+	    cm.replaceSelection(output);
+	    this.is_upload = false;
+		}
 	},
 	computed: {
     markdown_id: {
@@ -20,7 +55,22 @@ Vue.component('markdown', {
     }
   },
 	mounted: function() {
-		this.simplemde = new SimpleMDE({ element: document.getElementById(this.markdown_id) });
+		var self = this;
+		this.simplemde = new SimpleMDE({ 
+			element: document.getElementById(this.markdown_id),
+			toolbar: [
+				'bold', 'italic',
+				{
+					name: "custom",
+					action: function customFunction(editor){
+				  	self.editor = editor;
+				    self.is_upload = true;
+					},
+					className: "fa fa-star",
+					title: "Custom Button",
+				},
+			],
+		});
 		this.simplemde.value(this.content);
 
 		var _this = this;
