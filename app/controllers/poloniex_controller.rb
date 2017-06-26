eddcc51a1bd7267b5120f2bdf9e7f5d53ce97c53
robinvh85 <robinvh85@gmail.com>
2017-06-26 23:20:13 +0700

    Improve compare

--- app/controllers/poloniex_controller.rb
+++ app/controllers/poloniex_controller.rb
@@ -8,10 +8,11 @@ class PoloniexController < PoloniexBaseController
 	end
 
 	def compare_chart
-		base_unit = params[:base_unit]
-		base_unit = 'USDT' if base_unit.nil? || base_unit.empty?
+		@base_unit = params[:base_unit]
+		@base_unit = 'USDT' if @base_unit.nil? || @base_unit.empty?
 
-		@currency_pairs = CurrencyPair.where(base_unit: base_unit, is_init: 1).order(sort: 'asc')
+		@currency_pairs = CurrencyPair.where(base_unit: @base_unit, is_init: 1).order(sort: 'asc')
+		@currency_pairs = @currency_pairs.to_a.unshift(CurrencyPair.find_by(name: 'USDT_BTC'))
 	end
 
 end
